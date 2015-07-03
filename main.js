f8ee9f27049ee50e8ce192dd4f8064e53cd55129
Justin <jethielke@gmail.com>
2015-07-02 21:03:29 -0600

    my commit

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
-var firstReverse = function(string){
-	var output = string.reverse(string)
-	return output
+// var firstReverse = function(string) {
+//   var o = '';
+//   for (var i = string.length - 1; i >= 0; i--)
+//     o += string[i];
+//   return o;
+// }
+
+// console.log(firstReverse('hello'))
+
+
+
+//=============================================================================
+
+
+var swapCase = function(str){
+	
+	var string = str.split('')
+
+	for (var i = 0; i < string.length; i++) {
+		
+		if (string[i] === string[i].toLowerCase()){
+			string[i] === string[i].toUpperCase()
+		}
+		else {
+			string[i] === string[i].toLowerCase()
+		}
+	};
+		str = string.join()
+		console.log(str)
+		return str
 }
 
-console.log(firstReverse('hello'))
+swapCase('jUSTIN tHIELKE')
